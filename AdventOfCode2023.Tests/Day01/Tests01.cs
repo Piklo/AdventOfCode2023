ca44745f26f73d621ae3f695b271389a19bbc275
Piklo <16057987+Piklo@users.noreply.github.com>
2023-12-01 13:45:24 +0100

    Add project files.

---
+++ AdventOfCode2023.Tests/Day01/Tests01.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode2023.Day01;
+
+namespace AdventOfCode2023.Tests.Day01;
+
+public sealed class Tests01
+{
+    [Fact]
+    public void TestExampleP1()
+    {
+        const string example = """
+            1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet
+            """;
+
+        var res = Solution01.SolveP1(example);
+
+        Assert.Equal(12 + 38 + 15 + 77, res);
+    }
+
+    [Fact]
+    public void TestP1()
+    {
+        var res = Solution01.SolveP1(Input01.Data);
+
+        Assert.Equal(55447, res);
+    }
+
+    [Fact]
+    public void TestExampleP2()
+    {
+        const string example = """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+            """;
+
+        var res = Solution01.SolveP2(example);
+
+        Assert.Equal(29 + 83 + 13 + 24 + 42 + 14 + 76, res);
+    }
+
+    [Fact]
+    public void TestP2()
+    {
+        var res = Solution01.SolveP2(Input01.Data);
+
+        Assert.Equal(54706, res);
+    }
+}
---
+++ AdventOfCode2023/Day01/Solution01.cs
@@ -0,0 +1,131 @@
+﻿using System.Buffers;
+using System.Collections.Frozen;
+
+namespace AdventOfCode2023.Day01;
+
+public static class Solution01
+{
+    private static readonly FrozenDictionary<string, int> Numbers = new Dictionary<string, int>()
+    {
+        { "0", 0 },
+        { "zero", 0 },
+        { "1", 1 },
+        { "one", 1 },
+        { "2", 2 },
+        { "two", 2 },
+        { "3", 3 },
+        { "three", 3 },
+        { "4", 4 },
+        { "four", 4 },
+        { "5", 5 },
+        { "five", 5 },
+        { "6", 6 },
+        { "six", 6 },
+        { "7", 7 },
+        { "seven", 7 },
+        { "8", 8 },
+        { "eight", 8 },
+        { "9", 9 },
+        { "nine", 9 },
+    }.ToFrozenDictionary();
+
+    public static int SolveP1(string data)
+    {
+        var span = data.AsSpan();
+        var sum = 0;
+        var linesCount = span.Count('\n') + 1;
+        var destination = ArrayPool<Range>.Shared.Rent(linesCount);
+
+        try
+        {
+            ReadOnlySpan<Range> destinationSpan = destination.AsSpan()[..linesCount];
+            span.Split(destination, '\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            foreach (var range in destinationSpan)
+            {
+                var line = span[range.Start..range.End];
+                var firstDigit = 0;
+                var lastDigit = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (char.IsDigit(line[i]))
+                    {
+                        firstDigit = line[i] - '0';
+                        break;
+                    }
+                }
+
+                for (int i = line.Length - 1; i >= 0; i--)
+                {
+                    if (char.IsDigit(line[i]))
+                    {
+                        lastDigit = line[i] - '0';
+                        break;
+                    }
+                }
+
+                var number = (firstDigit * 10) + lastDigit;
+                sum += number;
+            }
+        }
+        finally
+        {
+            ArrayPool<Range>.Shared.Return(destination);
+        }
+
+        return sum;
+    }
+
+    public static int SolveP2(string data)
+    {
+        var span = data.AsSpan();
+        var sum = 0;
+        var linesCount = span.Count('\n') + 1;
+        var destination = ArrayPool<Range>.Shared.Rent(linesCount);
+
+        try
+        {
+            ReadOnlySpan<Range> destinationSpan = destination.AsSpan()[..linesCount];
+            span.Split(destination, '\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            foreach (var range in destinationSpan)
+            {
+                var line = span[range.Start..range.End];
+                var firstDigit = (Index: int.MaxValue, Value: -1);
+                var lastDigit = (Index: int.MinValue, Value: -1);
+
+                foreach (var kv in Numbers)
+                {
+                    var firstIndex = line.IndexOf(kv.Key);
+
+                    if (firstIndex == -1)
+                    {
+                        continue;
+                    }
+
+                    var lastIndex = line.LastIndexOf(kv.Key);
+
+                    if (firstIndex < firstDigit.Index || firstDigit.Value == -1)
+                    {
+                        firstDigit = (firstIndex, kv.Value);
+                    }
+
+                    if (lastIndex > lastDigit.Index || firstDigit.Value == -1)
+                    {
+                        lastDigit = (lastIndex, kv.Value);
+                    }
+                }
+
+                var number = (firstDigit.Value * 10) + lastDigit.Value;
+                sum += number;
+            }
+        }
+        finally
+        {
+            ArrayPool<Range>.Shared.Return(destination);
+        }
+
+        return sum;
+    }
+}
