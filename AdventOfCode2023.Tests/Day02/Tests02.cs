965bb9cb5c70ff1d1150f00b0c2775ac97c3e438
Piklo <16057987+Piklo@users.noreply.github.com>
2023-12-02 18:12:05 +0100

    added solution to day 2

---
+++ AdventOfCode2023.Tests/Day02/Tests02.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode2023.Day02;
+
+namespace AdventOfCode2023.Tests.Day02;
+
+public sealed class Tests02
+{
+    [Fact]
+    public void TestExampleP1()
+    {
+        const string input = """
+            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+            """;
+
+        var res = Solution02.SolveP1(input, 12, 13, 14);
+
+        Assert.Equal(8, res);
+    }
+
+    [Fact]
+    public void TestP1()
+    {
+        var res = Solution02.SolveP1(Input02.Data, 12, 13, 14);
+
+        Assert.Equal(2447, res);
+    }
+
+    [Fact]
+    public void TestExampleP2()
+    {
+        const string input = """
+            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+            """;
+
+        var res = Solution02.SolveP2(input);
+
+        Assert.Equal(48 + 12 + 1560 + 630 + 36, res);
+    }
+
+    [Fact]
+    public void TestP2()
+    {
+        var res = Solution02.SolveP2(Input02.Data);
+
+        Assert.Equal(56322, res);
+    }
+}
---
+++ AdventOfCode2023/Day02/Solution02.cs
@@ -0,0 +1,129 @@
+﻿using System.Buffers;
+
+namespace AdventOfCode2023.Day02;
+
+public static class Solution02
+{
+    public static int SolveP1(ReadOnlySpan<char> data, int maxRed, int maxGreen, int maxBlue)
+    {
+        var res = 0;
+
+        var linesCount = data.Count('\n') + 1;
+        var destination = ArrayPool<Range>.Shared.Rent(linesCount);
+
+        try
+        {
+            ReadOnlySpan<Range> ranges = destination.AsSpan()[..linesCount];
+            data.Split(destination, '\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            foreach (var range in ranges)
+            {
+                var line = data[range.Start..range.End];
+
+                var game = ParseGameP1(line);
+
+                if (game.MaxRed <= maxRed && game.MaxGreen <= maxGreen && game.MaxBlue <= maxBlue)
+                {
+                    res += game.Id;
+                }
+            }
+        }
+        finally
+        {
+            ArrayPool<Range>.Shared.Return(destination);
+        }
+
+        return res;
+    }
+
+    private static (int Id, int MaxRed, int MaxGreen, int MaxBlue) ParseGameP1(ReadOnlySpan<char> game)
+    {
+        var colonIndex = game.IndexOf(':');
+        var id = int.Parse(game["Game ".Length..game.IndexOf(':')]);
+        game = game[(colonIndex + 2)..];
+
+        var maxRed = 0;
+        var maxGreen = 0;
+        var maxBlue = 0;
+
+        var setsCount = game.Count(';') + 1;
+        var setsArray = ArrayPool<Range>.Shared.Rent(setsCount);
+
+        try
+        {
+            ReadOnlySpan<Range> setRanges = setsArray.AsSpan()[..setsCount];
+            game.Split(setsArray, ';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            foreach (var setRange in setRanges)
+            {
+                var set = game[setRange.Start..setRange.End];
+                var valuesCount = set.Count(',') + 1;
+                var valuesArray = ArrayPool<Range>.Shared.Rent(valuesCount);
+
+                try
+                {
+                    ReadOnlySpan<Range> valueRanges = valuesArray.AsSpan()[..valuesCount];
+                    set.Split(valuesArray, ',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                    foreach (var valueRange in valueRanges)
+                    {
+                        var valueSpan = set[valueRange.Start..valueRange.End];
+                        var spaceIndex = valueSpan.IndexOf(' ');
+                        var value = int.Parse(valueSpan[..spaceIndex]);
+                        var color = valueSpan[(spaceIndex + 1)..];
+                        if (color.SequenceEqual("red") && maxRed < value)
+                        {
+                            maxRed = value;
+                        }
+                        else if (color.SequenceEqual("green") && maxGreen < value)
+                        {
+                            maxGreen = value;
+                        }
+                        else if (color.SequenceEqual("blue") && maxBlue < value)
+                        {
+                            maxBlue = value;
+                        }
+                    }
+                }
+                finally
+                {
+                    ArrayPool<Range>.Shared.Return(valuesArray);
+                }
+            }
+        }
+        finally
+        {
+            ArrayPool<Range>.Shared.Return(setsArray);
+        }
+
+        return (id, maxRed, maxGreen, maxBlue);
+    }
+
+    public static int SolveP2(ReadOnlySpan<char> data)
+    {
+        var res = 0;
+
+        var linesCount = data.Count('\n') + 1;
+        var destination = ArrayPool<Range>.Shared.Rent(linesCount);
+
+        try
+        {
+            ReadOnlySpan<Range> ranges = destination.AsSpan()[..linesCount];
+            data.Split(destination, '\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            foreach (var range in ranges)
+            {
+                var line = data[range.Start..range.End];
+                var game = ParseGameP1(line);
+                var multiplied = game.MaxRed * game.MaxBlue * game.MaxGreen;
+                res += multiplied;
+            }
+        }
+        finally
+        {
+            ArrayPool<Range>.Shared.Return(destination);
+        }
+
+        return res;
+    }
+}
