1c44f673fda68eab9f9189d668e544725480653a
Piklo <16057987+Piklo@users.noreply.github.com>
2023-12-05 17:48:49 +0100

    added solution to day 4

---
+++ AdventOfCode2023.Tests/Day04/Tests04.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode2023.Day04;
+
+namespace AdventOfCode2023.Tests.Day04;
+
+public static class Tests04
+{
+    private const string Example = """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        """;
+
+    [Fact]
+    public static void TestExampleP1()
+    {
+        var res = Solution04.SolveP1(Example);
+
+        Assert.Equal(13, res);
+    }
+
+    [Fact]
+    public static void TestP1()
+    {
+        var res = Solution04.SolveP1(Input04.Data);
+
+        Assert.Equal(21105, res);
+    }
+
+    [Fact]
+    public static void TestExampleP2()
+    {
+        var res = Solution04.SolveP2(Example);
+
+        Assert.Equal(expected: 30, res);
+    }
+
+    [Fact]
+    public static void TestP2()
+    {
+        var res = Solution04.SolveP2(Input04.Data);
+
+        Assert.Equal(5329815, res);
+    }
+}
---
+++ AdventOfCode2023/Day04/Solution04.cs
@@ -0,0 +1,130 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Day04;
+
+public static partial class Solution04
+{
+    [GeneratedRegex("(?<=:.*)\\d+(?=.*\\|)")]
+    private static partial Regex GetWinningNumbersRegex();
+
+    [GeneratedRegex("(?<=\\|.*)\\d+")]
+    private static partial Regex GetNumbersRegex();
+
+    public static int SolveP1(string data)
+    {
+        var res = 0;
+        var cards = data.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        foreach (var card in cards)
+        {
+            var parsedCard = ParseCard(card);
+            var score = GetWinningScoreP1(parsedCard);
+            res += score;
+        }
+
+        return res;
+    }
+
+    public static int SolveP2(string data)
+    {
+        var res = 0;
+        var cards = data.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        var parsedCards = new ScratchCard[cards.Length];
+        var dict = new Dictionary<ScratchCard, List<ScratchCard>>();
+
+        for (var i = 0; i < cards.Length; i++)
+        {
+            var card = cards[i];
+            var parsedCard = ParseCard(card);
+            dict[parsedCard] = [];
+            parsedCards[i] = parsedCard;
+        }
+
+        for (var i = 0; i < parsedCards.Length; i++)
+        {
+            var card = parsedCards[i];
+            var winningCount = GetWinningCount(card);
+            var list = dict[card];
+
+            for (var j = i + 1; j < i + winningCount + 1 && j < parsedCards.Length; j++)
+            {
+                list.Add(parsedCards[j]);
+            }
+        }
+
+        var queue = new Queue<ScratchCard>();
+
+        foreach (var card in parsedCards)
+        {
+            queue.Enqueue(card);
+        }
+
+        while (queue.Count > 0)
+        {
+            res++;
+            var card = queue.Dequeue();
+            var wonCards = dict[card];
+            foreach (var wonCard in wonCards)
+            {
+                queue.Enqueue(wonCard);
+            }
+        }
+
+        return res;
+    }
+
+    private record ScratchCard(int[] WinningNumbers, int[] Numbers);
+
+    private static ScratchCard ParseCard(string card)
+    {
+        var winningNumbers = GetWinningNumbersRegex().Matches(card);
+        var numbers = GetNumbersRegex().Matches(card);
+
+        var winningArray = new int[winningNumbers.Count];
+        var numbersArray = new int[numbers.Count];
+
+        for (var i = 0; i < winningNumbers.Count; i++)
+        {
+            var number = int.Parse(winningNumbers[i].Value);
+            winningArray[i] = number;
+        }
+
+        for (var i = 0; i < numbers.Count; i++)
+        {
+            var number = int.Parse(numbers[i].Value);
+            numbersArray[i] = number;
+        }
+
+        var scratchCard = new ScratchCard(winningArray, numbersArray);
+
+        return scratchCard;
+    }
+
+    private static int GetWinningScoreP1(ScratchCard card)
+    {
+        var found = GetWinningCount(card);
+
+        if (found == 0)
+        {
+            return 0;
+        }
+
+        var score = 1;
+        score <<= found - 1;
+
+        return score;
+    }
+
+    private static int GetWinningCount(ScratchCard card)
+    {
+        var found = 0;
+        foreach (var number in card.Numbers)
+        {
+            if (card.WinningNumbers.Contains(number))
+            {
+                found++;
+            }
+        }
+
+        return found;
+    }
+}
