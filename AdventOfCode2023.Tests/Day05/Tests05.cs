7a236826a727c6a000fa29f5fa270343529fbe9b
Piklo <16057987+Piklo@users.noreply.github.com>
2023-12-05 19:51:45 +0100

    prepared some of the base things for day 5

---
+++ AdventOfCode2023.Tests/Day05/Tests05.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode2023.Day05;
+
+namespace AdventOfCode2023.Tests.Day05;
+
+public static class Tests05
+{
+    private const string Example = """
+        seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+
+        water-to-light map:
+        88 18 7
+        18 25 70
+
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+
+        humidity-to-location map:
+        60 56 37
+        56 93 4
+        """;
+
+    [Fact]
+    public static void TestExampleP1()
+    {
+        var res = Solution05.SolveP1(Example);
+
+        Assert.Equal(35, res);
+    }
+}
---
+++ AdventOfCode2023/Day05/Solution05.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Day05;
+
+public static partial class Solution05
+{
+    public static int SolveP1(string data)
+    {
+        var res = 0;
+        var seeds = GetSeeds(data);
+        var seedsToSoil = GetSeedToSoilMap(data);
+        return res;
+    }
+
+    private static int[] GetSeeds(string data)
+    {
+        var match = GetSeedsRegex().Matches(data);
+        var seeds = new int[match.Count];
+
+        for (var i = 0; i < match.Count; i++)
+        {
+            var seed = int.Parse(match[i].Value);
+            seeds[i] = seed;
+        }
+
+        return seeds;
+    }
+
+    private static Dictionary<int, int> GetSeedToSoilMap(string data)
+    {
+        var dict = new Dictionary<int, int>();
+        var matches = GetSeedToSoilRegex().Matches(data);
+        foreach (var match in (IEnumerable<Match>)matches)
+        {
+            var destination = int.Parse(match.Groups["destination"].ValueSpan);
+            var source = int.Parse(match.Groups["source"].ValueSpan);
+            var range = int.Parse(match.Groups["range"].ValueSpan);
+            for (var i = 0; i < range; i++)
+            {
+                dict[source + i] = destination + i;
+            }
+        }
+
+        return dict;
+    }
+
+    [GeneratedRegex("(?<=seeds:.*)\\d+")]
+    private static partial Regex GetSeedsRegex();
+
+    // copy and paste this regex for other maps
+    [GeneratedRegex("(?<=seed-to-soil map:\\s+.*\\s?)^\\s?(?<destination>\\d+).(?<source>\\d+).(?<range>\\d+)\\s?$", RegexOptions.Multiline)]
+    private static partial Regex GetSeedToSoilRegex();
+}
