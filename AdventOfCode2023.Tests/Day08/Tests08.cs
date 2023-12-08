2d767e902c44f6fdeabee63ba4716af021ad70dc
Piklo <16057987+Piklo@users.noreply.github.com>
2023-12-08 13:59:38 +0100

    added solution to day 8 part 1

---
+++ AdventOfCode2023.Tests/Day08/Tests08.cs
@@ -0,0 +1,74 @@
+﻿using AdventOfCode2023.Day08;
+
+namespace AdventOfCode2023.Tests.Day08;
+
+public static class Tests08
+{
+    private const string Example1P1 = """
+        RL
+
+        AAA = (BBB, CCC)
+        BBB = (DDD, EEE)
+        CCC = (ZZZ, GGG)
+        DDD = (DDD, DDD)
+        EEE = (EEE, EEE)
+        GGG = (GGG, GGG)
+        ZZZ = (ZZZ, ZZZ)
+        """;
+
+    private const string Example2P1 = """
+        LLR
+
+        AAA = (BBB, BBB)
+        BBB = (AAA, ZZZ)
+        ZZZ = (ZZZ, ZZZ)
+        """;
+
+    private const string ExampleP2 = """
+        LLR
+
+        AAA = (BBB, BBB)
+        BBB = (AAA, ZZZ)
+        ZZZ = (ZZZ, ZZZ)
+        """;
+
+    [Fact]
+    public static void TestExample1P1()
+    {
+        var res = Solution08.SolveP1(Example1P1);
+
+        Assert.Equal(2, res);
+    }
+
+    [Fact]
+    public static void TestExample2P1()
+    {
+        var res = Solution08.SolveP1(Example2P1);
+
+        Assert.Equal(6, res);
+    }
+
+    [Fact]
+    public static void TestP1()
+    {
+        var res = Solution08.SolveP1(Input08.Data);
+
+        Assert.Equal(21797, res);
+    }
+
+    [Fact]
+    public static void TestExampleP2()
+    {
+        var res = Solution08.SolveP2(ExampleP2);
+
+        Assert.Equal(6, res);
+    }
+
+    //[Fact]
+    //public static void TestP2()
+    //{
+    //    var res = Solution08.SolveP2(Input08.Data);
+
+    //    Assert.Equal(-1, res);
+    //}
+}
---
+++ AdventOfCode2023/Day08/Solution08.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Frozen;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Day08;
+
+public static partial class Solution08
+{
+    public static int SolveP1(string data)
+    {
+        var steps = 0;
+        var map = new Map(data);
+        var dictionary = new Dictionary<string, Node>();
+
+        foreach (var node in map.Nodes)
+        {
+            dictionary[node.Value] = node;
+        }
+
+        var current = dictionary["AAA"];
+        var last = dictionary["ZZZ"];
+
+        var sequenceIndex = 0;
+        while (current != last)
+        {
+            var instruction = map.Instruction[sequenceIndex];
+
+            current = dictionary[instruction == 'L' ? current.Left : current.Right];
+
+            steps++;
+            sequenceIndex++;
+            if (sequenceIndex >= map.Instruction.Length)
+            {
+                sequenceIndex = 0;
+            }
+        }
+
+        return steps;
+    }
+
+    public static int SolveP2(string data)
+    {
+        var steps = 0;
+
+        var map = new Map(data);
+        var dictionary = GetDictionary(map);
+
+        var current = map.Nodes.Where(node => node.Value[^1] == 'A').ToArray().AsSpan();
+
+        var sequenceIndex = 0;
+        //while (current.Any(node => node.Value[^1] != 'Z'))
+        while (AnyNodeNotOnZ(current))
+        {
+            var instruction = map.Instruction[sequenceIndex];
+
+            for (var i = 0; i < current.Length; i++)
+            {
+                var node = current[i];
+                current[i] = dictionary[instruction == 'L' ? node.Left : node.Right];
+            }
+
+            steps++;
+            sequenceIndex++;
+            if (sequenceIndex >= map.Instruction.Length)
+            {
+                sequenceIndex = 0;
+            }
+        }
+
+        return steps;
+    }
+
+    private static FrozenDictionary<string, Node> GetDictionary(Map map)
+    {
+        var dictionary = new Dictionary<string, Node>();
+
+        foreach (var node in map.Nodes)
+        {
+            dictionary[node.Value] = node;
+        }
+
+        return dictionary.ToFrozenDictionary();
+    }
+
+    private static bool AnyNodeNotOnZ(ReadOnlySpan<Node> nodes)
+    {
+        for (var i = 0; i < nodes.Length; i++)
+        {
+            if (nodes[i].Value[^1] != 'Z')
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private partial class Map
+    {
+        public string Instruction { get; }
+
+        public Node[] Nodes { get; }
+
+        public Map(string data)
+        {
+            ReadOnlySpan<string> lines = data.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).AsSpan();
+            Instruction = lines[0];
+
+            lines = lines[1..];
+            Nodes = new Node[lines.Length];
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var values = NodeValuesRegex().Matches(lines[i]);
+                var value = values[0].Value;
+                var left = values[1].Value;
+                var right = values[2].Value;
+
+                var node = new Node(value, left, right);
+                Nodes[i] = node;
+            }
+        }
+
+        [GeneratedRegex("\\w+")]
+        private static partial Regex NodeValuesRegex();
+    }
+
+    private readonly record struct Node(string Value, string Left, string Right);
+}
