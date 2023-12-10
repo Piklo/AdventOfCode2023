17ff95f5efd8c1dbd3057bcdb7b034f39375d3d4
Piklo <16057987+Piklo@users.noreply.github.com>
2023-12-10 13:34:39 +0100

    added solution to day 10 part 1

---
+++ AdventOfCode2023.Tests/Day10/Tests10.cs
@@ -0,0 +1,108 @@
+﻿using AdventOfCode2023.Day10;
+
+namespace AdventOfCode2023.Tests.Day10;
+
+public static class Tests10
+{
+    [Fact]
+    public static void TestExample1P1()
+    {
+        const string example = """
+            .....
+            .S-7.
+            .|.|.
+            .L-J.
+            .....
+            """;
+
+        var res = Solution10.SolveP1(example);
+
+        Assert.Equal(4, res);
+    }
+
+    [Fact]
+    public static void TestExample2P1()
+    {
+        const string example = """
+            ..F7.
+            .FJ|.
+            SJ.L7
+            |F--J
+            LJ...
+            """;
+
+        var res = Solution10.SolveP1(example);
+
+        Assert.Equal(8, res);
+    }
+
+    [Fact]
+    public static void TestP1()
+    {
+        var res = Solution10.SolveP1(Input10.Data);
+
+        Assert.Equal(6640, res);
+    }
+
+    //[Fact]
+    //public static void TestExample1P2()
+    //{
+    //    const string example = """
+    //        ...........
+    //        .S-------7.
+    //        .|F-----7|.
+    //        .||.....||.
+    //        .||.....||.
+    //        .|L-7.F-J|.
+    //        .|..|.|..|.
+    //        .L--J.L--J.
+    //        ...........
+    //        """;
+
+    //    var res = Solution10.SolveP2(example);
+
+    //    Assert.Equal(4, res);
+    //}
+
+    //[Fact]
+    //public static void TestExample2P2()
+    //{
+    //    const string example = """
+    //        .F----7F7F7F7F-7....
+    //        .|F--7||||||||FJ....
+    //        .||.FJ||||||||L7....
+    //        FJL7L7LJLJ||LJ.L-7..
+    //        L--J.L7...LJS7F-7L7.
+    //        ....F-J..F7FJ|L7L7L7
+    //        ....L7.F7||L7|.L7L7|
+    //        .....|FJLJ|FJ|F7|.LJ
+    //        ....FJL-7.||.||||...
+    //        ....L---J.LJ.LJLJ...
+    //        """;
+
+    //    var res = Solution10.SolveP2(example);
+
+    //    Assert.Equal(8, res);
+    //}
+
+    //[Fact]
+    //public static void TestExample3P2()
+    //{
+    //    const string example = """
+    //        FF7FSF7F7F7F7F7F---7
+    //        L|LJ||||||||||||F--J
+    //        FL-7LJLJ||||||LJL-77
+    //        F--JF--7||LJLJ7F7FJ-
+    //        L---JF-JLJ.||-FJLJJ7
+    //        |F|F-JF---7F7-L7L|7|
+    //        |FFJF7L7F-JF7|JL---7
+    //        7-L-JL7||F7|L7F-7F7|
+    //        L.L7LFJ|||||FJL7||LJ
+    //        L7JLJL-JLJLJL--JLJ.L
+    //        """;
+
+    //    var res = Solution10.SolveP2(example);
+
+    //    Assert.Equal(10, res);
+    //}
+}
---
+++ AdventOfCode2023/Day10/Solution10.cs
@@ -0,0 +1,175 @@
+﻿namespace AdventOfCode2023.Day10;
+
+public static class Solution10
+{
+    public static int SolveP1(string data)
+    {
+        var lines = data.Split('\n', StringSplitOptions.TrimEntries);
+
+        var (i, j) = GetStartingPosition(lines);
+
+        _ = IsLooped(lines, i, j, i - 1, j, out var length) // north
+           || IsLooped(lines, i, j, i + 1, j, out length) // south
+           || IsLooped(lines, i, j, i, j + 1, out length) // east
+           || IsLooped(lines, i, j, i, j - 1, out length); // west
+
+        return length / 2;
+    }
+
+    private static (int I, int J) GetStartingPosition(ReadOnlySpan<string> lines)
+    {
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            var index = line.IndexOf('S');
+
+            if (index == -1)
+            {
+                continue;
+            }
+
+            return (i, index);
+        }
+
+        return (-1, -1);
+    }
+
+    private static bool IsLooped(ReadOnlySpan<string> lines, int startingI, int startingJ, int newI, int newJ, out int length)
+    {
+        var previousI = startingI;
+        var previousJ = startingJ;
+        var steps = 0;
+
+        while (newI != startingI || newJ != startingJ)
+        {
+            var currentI = newI;
+            var currentJ = newJ;
+
+            if (!MoveNext(lines, previousI, previousJ, currentI, currentJ, out newI, out newJ))
+            {
+                // not connected anywhere but to the previous point, therefore there is no loop
+                length = default;
+                return false;
+            }
+
+            previousI = currentI;
+            previousJ = currentJ;
+            steps++;
+        }
+
+        length = steps + 1;
+        return true;
+    }
+
+    private static bool MoveNext(ReadOnlySpan<string> lines, int previousI, int previousJ, int currentI, int currentJ, out int newI, out int newJ)
+    {
+        if (currentI - 1 != previousI && IsConnectedNorth(lines, currentI, currentJ))
+        {
+            newI = currentI - 1;
+            newJ = currentJ;
+            return true;
+        }
+        else if (currentI + 1 != previousI && IsConnectedSouth(lines, currentI, currentJ))
+        {
+            newI = currentI + 1;
+            newJ = currentJ;
+            return true;
+        }
+        else if (currentJ - 1 != previousJ && IsConnectedWest(lines, currentI, currentJ))
+        {
+            newI = currentI;
+            newJ = currentJ - 1;
+            return true;
+        }
+        else if (currentJ + 1 != previousJ && IsConnectedEast(lines, currentI, currentJ))
+        {
+            newI = currentI;
+            newJ = currentJ + 1;
+            return true;
+        }
+        else
+        {
+            newI = default;
+            newJ = default;
+            return false;
+        }
+    }
+
+    private static bool IsConnectedNorth(ReadOnlySpan<string> lines, int i, int j)
+    {
+        return ConnectsNorth(lines, i, j) && ConnectsSouth(lines, i - 1, j);
+    }
+
+    private static bool IsConnectedEast(ReadOnlySpan<string> lines, int i, int j)
+    {
+        return ConnectsEast(lines, i, j) && ConnectsWest(lines, i, j + 1);
+    }
+
+    private static bool IsConnectedSouth(ReadOnlySpan<string> lines, int i, int j)
+    {
+        return ConnectsSouth(lines, i, j) && ConnectsNorth(lines, i + 1, j);
+    }
+
+    private static bool IsConnectedWest(ReadOnlySpan<string> lines, int i, int j)
+    {
+        return ConnectsWest(lines, i, j) && ConnectsEast(lines, i, j - 1);
+    }
+
+    private static bool ConnectsNorth(ReadOnlySpan<string> lines, int i, int j)
+    {
+        if (!IsInbounds(lines, i, j))
+        {
+            return false;
+        }
+
+        var tile = lines[i][j];
+
+        return tile is 'S' or '|' or 'L' or 'J';
+    }
+
+    private static bool ConnectsEast(ReadOnlySpan<string> lines, int i, int j)
+    {
+        if (!IsInbounds(lines, i, j))
+        {
+            return false;
+        }
+
+        var tile = lines[i][j];
+
+        return tile is 'S' or '-' or 'L' or 'F';
+    }
+
+    private static bool ConnectsSouth(ReadOnlySpan<string> lines, int i, int j)
+    {
+        if (!IsInbounds(lines, i, j))
+        {
+            return false;
+        }
+
+        var tile = lines[i][j];
+
+        return tile is 'S' or '|' or '7' or 'F';
+    }
+
+    private static bool ConnectsWest(ReadOnlySpan<string> lines, int i, int j)
+    {
+        if (!IsInbounds(lines, i, j))
+        {
+            return false;
+        }
+
+        var tile = lines[i][j];
+
+        return tile is 'S' or '-' or 'J' or '7';
+    }
+
+    private static bool IsInbounds(ReadOnlySpan<string> lines, int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= lines.Length || j >= lines[i].Length)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
